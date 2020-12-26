38883b68cd07b0b90d81b74dc36c5a04d0f945db
Ubuntu <ubuntu@ip-172-31-92-61.ec2.internal>
2020-12-26 07:29:00 +0000

    pose points generated

--- code/pipeline_human_pose_points.py
+++ code/pipeline_human_pose_points.py
@@ -15,8 +15,42 @@
 
 from functions import general_functions as gf
 from functions import db_functions as db
+import tflite_runtime.interpreter as tflite
+from PIL import Image
+import numpy as np
+
 
 import sys, os, argparse
+
+def parse_pose_output(heatmap_data,offset_data, threshold):
+
+  '''
+  Input:
+    heatmap_data - hetmaps for an image. Three dimension array
+    offset_data - offset vectors for an image. Three dimension array
+    threshold - probability threshold for the keypoints. Scalar value
+  Output:
+    array with coordinates of the keypoints and flags for those that have
+    low probability
+  '''
+
+  joint_num = heatmap_data.shape[-1]
+  pose_kps = np.zeros((joint_num,3), np.uint32)
+
+  for i in range(heatmap_data.shape[-1]):
+
+      joint_heatmap = heatmap_data[...,i]
+      max_val_pos = np.squeeze(np.argwhere(joint_heatmap==np.max(joint_heatmap)))
+      remap_pos = np.array(max_val_pos/8*257,dtype=np.int32)
+      pose_kps[i,0] = int(remap_pos[0] + offset_data[max_val_pos[0],max_val_pos[1],i])
+      pose_kps[i,1] = int(remap_pos[1] + offset_data[max_val_pos[0],max_val_pos[1],i+joint_num])
+      max_prob = np.max(joint_heatmap)
+
+      if max_prob > threshold:
+        if pose_kps[i,0] < 257 and pose_kps[i,1] < 257:
+          pose_kps[i,2] = 1
+
+  return pose_kps
 
 def main_human_pose_pipeline(argv):
     #  Parse argv to get the video file and the rotate angle (if needed)
@@ -49,11 +83,64 @@ def main_human_pose_pipeline(argv):
     
     critical_frames_df = db.get_critical_frames(video_file, model_name, model_version, 0.70)
     
+    if (critical_frames_df.shape[0] == 0):
+        print ("Cannot find critical frames for {} in db. Exiting.")
+        sys.exit(1)
     
     
+    #Step 3.Perform pose point detection on critical frames
     
+    # Download the model from S3
+    model_file = 'posenet_v1.tflite'
+    model_download_path = os.path.join(temp_dir, model_file)
+    bucket = 'w-yrs-models'
+    
+    if not gf.download_file_from_s3(bucket,model_file, model_download_path):
+        print ("Could not download model file from S3. Exiting..")
+        sys.exit(1)
 
+    tflite_interpreter = tflite.Interpreter(model_path=model_download_path)
+    tflite_interpreter.allocate_tensors()
 
+    # Get input and output tensors.
+    input_details = tflite_interpreter.get_input_details()
+    output_details = tflite_interpreter.get_output_details()
+    
+    #print("Type of input:", input_details[0]['dtype'])
+    
+    # NxHxWxC, H:1, W:2
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+    
+    print("Height {}, Width {}".format(height, width))
+    img = Image.open('./tmp/IMG_3770_FRAME_65.png').resize((width, height))
+    
+    # add N dim
+    input_data = np.expand_dims(img, axis=0)
+    
+    input_mean = 127.5
+    input_std = 127.5
+    
+    input_data = (np.float32(input_data) - input_mean) / input_std
+    tflite_interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    #start_time = time.time()
+    tflite_interpreter.invoke()
+    #stop_time = time.time()
+
+    output_data = tflite_interpreter.get_tensor(output_details[0]['index'])
+    offset_data = tflite_interpreter.get_tensor(output_details[1]['index'])
+    
+    # Getting rid of the extra dimension
+    template_heatmaps = np.squeeze(output_data)
+    template_offsets = np.squeeze(offset_data)
+    
+    print("template_heatmaps' shape:", template_heatmaps.shape)
+    print("template_offsets' shape:", template_offsets.shape)
+    
+    template_kps = parse_pose_output(template_heatmaps,template_offsets,0.3)
+    
+    print(template_kps)
 
 
 if __name__ == "__main__":
